85f758ee67d9d3dd94ab7ef323eff2dab458c415
vith0rv32 <161614566+vith0rv32@users.noreply.github.com>
2024-10-31 10:51:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentloaded", () => {
+
+const cards = [
+    {
+        name:'android'
+        img:'images/android.png',    
+    },
+    {
+        name:'chrome'
+        img:'images/chrome.png',    
+    }, 
+    {
+        name:'git'
+        img:'images/git.png',    
+    }, 
+    {
+        name:'stackoverflow'
+        img:'images/stackoverflow.png',    
+    }, 
+    {
+        name:'linux'
+        img:'images/linux.png',    
+    }, 
+    {
+        name:'github'
+        img:'images/github.png',    
+    },  {
+        name:'android'
+        img:'images/android.png',    
+    },
+    {
+        name:'chrome'
+        img:'images/chrome.png',    
+    }, 
+    {
+        name:'git'
+        img:'images/git.png',    
+    }, 
+    {
+        name:'stackoverflow'
+        img:'images/stackoverflow.png',    
+    }, 
+    {
+        name:'linux'
+        img:'images/linux.png',    
+    }, 
+    {
+        name:'github'
+        img:'images/github.png',    
+    }
+]
+ 
+  //embaralhar todas as cartas
+
+});
