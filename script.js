82339d91ccebf827445bca313035c92ea5c6d113
vith0rv32 <161614566+vith0rv32@users.noreply.github.com>
2024-11-05 11:42:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -51,5 +51,13 @@ const cards = [
 ]
  
   //embaralhar todas as cartas
+  cards.sort(() => 0.5 - Math.random());
+  //carregar elementos html no script
+  const board = document.querySelector('.board');
+  const resulView = document.querySelector('#result')
+let cardsChosen = []; //cartas escolhidas
+let cardsChosenId = []; //id das cartas escolhidas
+let cardsWon = [] //cartas combinadas 
+
 
 });
